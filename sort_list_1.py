38bf0cd3b44088904e3017e8340af8ca283dc642
zhrcosta <guilherme.di.aquino@gmail.com>
2021-03-10 20:24:21 -0300

    Verificação de oredem crescente alterada

--- sort_list_1.py
+++ sort_list_1.py
@@ -7,29 +7,28 @@ def sort_list(lista):
 
     while not count == len(s_lista)-1:
 
-        count = 0
+        # Verifica se todos os valores estão em ordem crescente.
+        for index in range(len(s_lista)- 1 ):
+            if s_lista[index] <= s_lista[index + 1]:
+                count += 1
+            
+        if count == len(s_lista) - 1:
+            return s_lista, loop
+            break
+        else:
+            count = 0
 
         # Altera a ordem dos elementos da lista
         for index in range(len(s_lista)-1):
-            if s_lista[index] > s_lista[index+1]:
-                s_lista[index], s_lista[index +
-                                        1] = s_lista[index+1], s_lista[index]
+            if s_lista[index] > s_lista[index + 1]:
+                s_lista[index], s_lista[index + 1] = s_lista[index + 1], s_lista[index]
 
-        # Verifica se todos os valores estão em ordem crescente.
-        for index in range(len(s_lista)-1):
-            if s_lista[index] <= s_lista[index + 1]:
-                count += 1
 
         loop += 1
 
-    print(2*'\n')
-    print(f'Repetições do laço: {loop}')
-    print(f'Comprimento da lista: {len(s_lista)}')
-    print(s_lista)
-    print(2*'\n')
-
-    return s_lista
+    return s_lista, loop
 
 l1 = [2, 3, 2, 5, 72, 6, 1, 8, 1, 2, 3, 4, 5, 7, 1, 2, 1, 10, 2, 23, 0]
 
-sort_list(l1)
+
+print(sort_list(l1))
