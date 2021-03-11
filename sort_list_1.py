2e3ebfa3bf267c20c95d4ab30086e97afb06a645
zhrcosta <guilherme.di.aquino@gmail.com>
2021-03-11 17:51:54 -0300

    corrigindo tipos mutaveis

--- sort_list_1.py
+++ sort_list_1.py
@@ -1,34 +1,31 @@
 
 def sort_list(lista):
 
-    s_lista = lista
     count = 0
     loop = 0
 
-    while not count == len(s_lista)-1:
+    while not count == len(lista)-1:
 
         # Verifica se todos os valores estão em ordem crescente.
-        for index in range(len(s_lista)- 1 ):
-            if s_lista[index] <= s_lista[index + 1]:
+        for index in range(len(lista) - 1):
+            if lista[index] <= lista[index + 1]:
                 count += 1
-            
-        if count == len(s_lista) - 1:
-            return s_lista, loop
+
+        if count == len(lista) - 1:
             break
         else:
             count = 0
 
         # Altera a ordem dos elementos da lista
-        for index in range(len(s_lista)-1):
-            if s_lista[index] > s_lista[index + 1]:
-                s_lista[index], s_lista[index + 1] = s_lista[index + 1], s_lista[index]
-
+        for index in range(len(lista)-1):
+            if lista[index] > lista[index + 1]:
+                lista[index], lista[index + 1] = lista[index + 1], lista[index]
 
         loop += 1
 
-    return s_lista, loop
 
 l1 = [2, 3, 2, 5, 72, 6, 1, 8, 1, 2, 3, 4, 5, 7, 1, 2, 1, 10, 2, 23, 0]
 
-
-print(sort_list(l1))
+print(l1)
+sort_list(l1)
+print(l1)
